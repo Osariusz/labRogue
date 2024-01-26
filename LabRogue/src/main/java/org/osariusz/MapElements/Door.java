2c538f7954d26de3d5ad605ebc1aebf144de48a1
Osariusz <osariusz@interia.pl>
2024-01-26 12:35:15 +0100

    Player sight

--- LabRogue/src/main/java/org/osariusz/MapElements/Door.java
+++ LabRogue/src/main/java/org/osariusz/MapElements/Door.java
@@ -1,8 +1,12 @@
 package org.osariusz.MapElements;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@Getter
 @SuperBuilder(toBuilder = true)
+@NoArgsConstructor
 public class Door extends MapElement {
 
     @Override
