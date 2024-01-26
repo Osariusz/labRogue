6fcf971d844fe3dd200b891a20814764292f4731
Osariusz <osariusz@interia.pl>
2024-01-26 12:34:46 +0100

    Door

--- LabRogue/src/main/java/org/osariusz/MapElements/Door.java
+++ LabRogue/src/main/java/org/osariusz/MapElements/Door.java
@@ -4,4 +4,12 @@ import lombok.experimental.SuperBuilder;
 
 @SuperBuilder(toBuilder = true)
 public class Door extends MapElement {
+
+    @Override
+    protected void initializeDefaults() {
+        super.initializeDefaults();
+        super.name = "Door";
+        super.id = "door";
+        super.symbol = '▯';
+    }
 }
