48fbb4387f7616d802e15ba910743bd50a4fbfbd
Osariusz <osariusz@interia.pl>
2024-01-30 13:37:53 +0100

    Help text update

--- LabRogue/src/main/java/org/osariusz/Graphics/IO.java
+++ LabRogue/src/main/java/org/osariusz/Graphics/IO.java
@@ -16,8 +16,8 @@ public class IO {
 
     String helpText = """
             w s a d - move
-            e [slotNumber] [slotName] - equip item on slotNumber from backpack to slotName
-            de [slotNumber] [slotName] - deequip item on position slotNumber from slotName
+            e [backpackSlotNumber] [slotName] [equipmentSlotNumber] - equip item on backpackSlotNumber from backpack to slotName on position equipmentSlotNumber
+            de [equipmentSlotNumber] [slotName] [backpackSlotNumber] - deequip item on position equipmentSlotNumber from slotName to backpackSlotNumber
             f [wsad] - fire/attack on selected position 
             """;
 
